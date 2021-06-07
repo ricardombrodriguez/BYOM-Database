6fe06e5ddd140b9b1dd33ccf83b4fba892094939
goncalo-leal <goncalolealsilva@ua.pt>
2021-06-07 18:49:34 +0100

    criar grupo, insert na bd

--- C#Template/ProjetoFinalBD/CriarGrupo.cs
+++ C#Template/ProjetoFinalBD/CriarGrupo.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ProjetoFinalBD
 {
     public partial class CriarGrupo : Form
     {
-        public CriarGrupo()
+        private SqlConnection cn;
+        private string id;
+
+        public CriarGrupo(string id = "")
         {
             InitializeComponent();
+
+            if (id == "")
+            {
+                btnApagarGrupo.Visible = false;
+
+                // carregar todas as cadeiras e meter na combo
+            }
+
+            else
+            {
+                // deixar o botão visivel,
+                // carregar os dados para as caixas de texto e meter o id numa text escondida
+            }
+        }
+
+        private void CriarGrupo_Load(object sender, EventArgs e)
+        {
+            cn = getSGBDConnection();
+        }
+
+        private SqlConnection getSGBDConnection()
+        {
+            return new SqlConnection("Data Source=tcp:mednat.ieeta.pt\\SQLSERVER,8101;Persist Security Info=True;User ID=p9g5;Password=-737279605@BD;");
+        }
+
+        private bool verifySGBDConnection()
+        {
+            if (cn == null)
+                cn = getSGBDConnection();
+
+            if (cn.State != ConnectionState.Open)
+                cn.Open();
+
+            return cn.State == ConnectionState.Open;
         }
 
         private void btnApagarGrupo_Click(object sender, EventArgs e)
@@ -26,9 +64,48 @@ namespace ProjetoFinalBD
 
         private void btnGuardarGrupo_Click(object sender, EventArgs e)
         {
-            FormState.PreviousPage.Show();
-            this.Hide();
-            FormState.PreviousPage = this;
+            if (grupo_nome.TextLength == 0 || grupo_cadeira.Text.Length == 0)
+            {
+                MessageBox.Show("Os campos têm de estar preenchidos.", "Erro",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // ver também o que fazer com as listBoxes
+
+                if (!verifySGBDConnection())
+                    return;
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = "INSERT PROJETO.Grupo VALUES (@nome, @cadeira, @codigo_criador) ";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@nome", grupo_nome.Text);
+                cmd.Parameters.AddWithValue("@cadeira", grupo_cadeira.SelectedValue);
+                cmd.Parameters.AddWithValue("@codigo_criador", "");
+                cmd.Connection = cn;
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed to insert Group in database. \n ERROR MESSAGE: \n" + ex.Message);
+                }
+                finally
+                {
+                    cn.Close();
+                }
+
+                MessageBox.Show("", "Grupo Adicionado",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                FormState.PreviousPage.Show();
+                this.Hide();
+                FormState.PreviousPage = this;
+            }
+            
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -49,5 +126,6 @@ namespace ProjetoFinalBD
             ModalProfessor professor = new ModalProfessor();
             professor.Show();
         }
+
     }
 }
